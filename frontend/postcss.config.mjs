bc83e790a51fe043414c9081b5e52ac89c84c20a
quannguyenhai2001 <nguyenhaiquan.0121@gmail.com>
2025-08-06 09:23:49 +0700

    feat: add shadcn/ui

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,6 +1,5 @@
-/** @type {import('postcss').Config} */
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="p-8 flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-tertiary font-bold text-3xl mb-4">Xin chào! 👋</h1>
-      <p>Trang sử dụng Tailwind CSS v4 với các biến design token</p>
-      <div className="bg-gradient mt-4 p-4 rounded-md text-white">
-        Gradient background mẫu
-      </div>
-    </main>
+    <div>
+      <Button variant="destructive">Click Me</Button>
+    </div>
   );
 }
